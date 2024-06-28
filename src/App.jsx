0883a54ccd4ae229d4bc5a4f93a740930636e0ef
LUC4S4L <lucas.aliaga.m@utec.edu.pe>
2024-06-28 08:43:07 -0500

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
           <Route path="/login" element={<Login/>}/>
           <Route path="/register" element={<Register/>}/>
           <Route path="*" element={<Navigate to="/login"/>}/>
+          <Route path="/home" element={<Home/>}/>
         </Routes>
       </Router>
     </>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Home = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default Home

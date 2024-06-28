0bb2888e172be05f3167fd9461402d897d4d4388
LUC4S4L <lucas.aliaga.m@utec.edu.pe>
2024-06-28 07:42:33 -0500

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router,Routes,Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import CreateItem from './pages/CreateItem'
+import UpdateItem from './pages/UpdateItem'
+
 
 function App() {
 
   return (
     <>
-      
+      <Router>
+        <Routes>
+        <Route path="/" element={<Navigate to="/login"/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/items" element={<Items/>}/>
+          <Route path="/create-item" element={<CreateItem/>}/>
+          <Route path="/item/:id" element={<UpdateItem/>}/>
+          <Route path="/cart" element={<Cart/>}/>
+          <Route path="*" element={<Navigate to="/login"/>}/>
+        </Routes>
+      </Router>
     </>
   )
 }
